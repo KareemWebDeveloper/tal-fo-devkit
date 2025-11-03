a48f281935f4d468f537fc389af3fabda3f26bf9
Kareem Ayman <admin@kareemdev.com>
2025-11-03 13:19:26 +0200

    update apiclient

--- apps/admin-example/src/pkg/api/apiClient.ts
+++ apps/admin-example/src/pkg/api/apiClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@connectrpc/connect";
 import { AuthInterceptor, ErrorInterceptor } from "@devkitvue/apiclient";
 import { createConnectTransport } from "@connectrpc/connect-web";
-import { DevkitService } from "@buf/ahmeddarwish_devkit-api.bufbuild_es/devkit/v1/devkit_service_pb";
+import { TalService } from "@buf/mawhub_tal.bufbuild_es/tal/v1/tal_service_pb";
 import router from "@/pkg/router";
 const transport = createConnectTransport({
   baseUrl: import.meta.env.VITE_API_URL,
@@ -28,4 +28,25 @@ const transport = createConnectTransport({
   useHttpGet: true,
 });
 
-export const apiClient = createClient(DevkitService, transport);
+export const apiClient = createClient(TalService, transport);
+
+
+// import { Client, createClient } from "@connectrpc/connect";
+// import { createConnectTransport } from "@connectrpc/connect-web";
+// import { TalService } from "@buf/mawhub_tal.bufbuild_es/tal/v1/tal_service_pb";
+
+// const transport = createConnectTransport({
+//   baseUrl: import.meta.env.VITE_API_URL,
+//   fetch: (input, init) => {
+//     return fetch(input, {
+//       ...init,
+//       credentials: "include", // 👈 inject credentials here
+//     });
+//   },
+//   useHttpGet: true,
+// });
+
+// export const apiClient: Client<typeof TalService> = createClient(
+//   TalService,
+//   transport,
+// );
